45ee3f34c88f2aaaf6852947876a0a70bd852221
Jelinek-J <34974563+Jelinek-J@users.noreply.github.com>
2026-07-28 14:55:25 +0200

    Update read_counts.cpp metadata and formatting
    Updated the last update date and fixed formatting issues.

--- Cpp_sources/read_counts.cpp
+++ Cpp_sources/read_counts.cpp
@@ -1,5 +1,5 @@
 ﻿// Created by Jan Jelínek (jan.jelinek@biomed.cas.cz)
-// Last update: 2023-03-29
+// Last update: 2026-07-28
 // Released under Apache License 2.0
 
 #include <iostream>
@@ -10,13 +10,13 @@ int main(int argc, char* argv[]) {
   if (argc != 1) {
     std::cout << "read_counts\t Read file in SAM format from standard input, group reads by RNAME and POS and\n";
     std::cout << "           \t print read counts to standard output in TAB-separated values file format.\n";
-    std::cout << "Created by Jan Jelínek (jan.jelinek@biomed.cas.cz); last update: 2023-03-29; license: Apache License 2.0" << std::endl;
+    std::cout << "Created by Jan Jelínek (jan.jelinek@biomed.cas.cz); last update: 2026-07-28; license: Apache License 2.0" << std::endl;
     return 0;
   }
 
   std::map<std::string, std::map<size_t, size_t>> counts;
   for (std::string line; std::getline(std::cin, line); ) {
-    if (line.size() > 0 && line[0] != '@') {
+    if (!line.empty() && line[0] != '@') {
       std::stringstream parts(line);
       std::string part;
       std::string rname;
